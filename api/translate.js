bd637cfd215f2c41487ccf9fec7326e9d750cd2d
Burak Kenan <kenan-burak-kenan@hotmail.com>
2025-12-04 16:08:09 +0100

    sadasd

--- api/translate.js
+++ api/translate.js
@@ -1,4 +1,5 @@
-// api/translate.js
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Only POST allowed' });
